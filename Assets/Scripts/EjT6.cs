eb285b8ee55f1243edadca00cb33c28248b6637b
iangerkovich <101577604+iangerkovich@users.noreply.github.com>
2022-04-12 10:19:43 -0300

    avanzado

---
+++ Assets/Scripts/EjT6.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EjT6 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public int entcampo;
+    public int entplatea;
+
+    void Start()
+    {
+        int preciocampo;
+        preciocampo = entcampo * 1200;
+        int precioplatea;
+        precioplatea = entplatea * 2000;
+        int enttotal;
+        enttotal = preciocampo + precioplatea;
+        int captotal;
+        captotal = entcampo + entplatea;
+
+
+
+        Debug.Log("Ingrese la cantidad de entradas vendidas en sector campo");
+
+        if (entcampo <= 20400 && entcampo > 0)
+
+        {
+            Debug.Log("Se recaudaron $" + preciocampo + " solo con las entradas de sector campo");
+        }
+        else
+        {
+            Debug.Log("No es posible que se hayan vendido tantas entradas");
+        }
+
+        Debug.Log("Ingrese la cantidad de entradas vendidas en sector platea");
+
+        if (entplatea <= 16200 && entplatea > 0)
+        {
+            Debug.Log("Se recaudaron $" + precioplatea + " solo con las entradas de sector platea. El total de plata recaudada sumando secotr campo y platea fue $" + enttotal);
+
+        }
+
+        else
+        {
+            Debug.Log("No es posible que se hayan vendido tantas entradas");
+        }
+
+        if (captotal < 36600)
+        {
+            Debug.Log("Quedaron " + captotal);
+
+        }
+
+        else if (captotal == 36600)
+        {
+            Debug.Log("Sold out!");
+
+        }
+        else
+        {
+            Debug.Log("No se pudieron vender tantas entradas");
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
